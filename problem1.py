978a086311357dad5f607cc79638e2a9565de7a4
phoenitunes <70701513+phoenitunes@users.noreply.github.com>
2022-10-12 11:24:25 +0100

    first problem

---
+++ problem1.py
@@ -0,0 +1,24 @@
+# Multiples of 3 or 5
+# Show HTML problem content 
+# Problem 1
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+from audioop import mul
+
+
+def multiplies3_5( limit):
+    sum=0
+    for i in range(limit):
+        if i % 3 == 0 or i %5==0:
+            sum=sum+i
+    return print(sum)
+
+
+multiplies3_5(10)         
+multiplies3_5(1000)         
+
+    
