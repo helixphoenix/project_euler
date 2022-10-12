9c3722343ebdb5978289735a84b123d1e6de248e
phoenitunes <70701513+phoenitunes@users.noreply.github.com>
2022-10-12 16:49:22 +0100

    largest prime factor

---
+++ problem3.py
@@ -0,0 +1,16 @@
+# Largest prime factor
+# Show HTML problem content 
+# Problem 3
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+
+
+def prime_fact(num):
+    primes=[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    for i in reversed(primes):
+        if num % i == 0:
+            return print(i)
+                
+prime_fact(600851475143)        
